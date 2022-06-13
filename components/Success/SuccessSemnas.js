c7b8cd6f5c490deb5948f472d699e7ccc5882371
shonsirsha <seangeekpro@gmail.com>
2022-06-13 18:25:43 +0200

    Sharing session register done

---
+++ components/Success/SuccessSemnas.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import { mediaBreakpoint } from "utils/mediaBreakpoints";
+import { SemiCircle } from "components/SemiCircle";
+
+const OuterContainer = styled.div`
+	display: flex;
+	border-top-right-radius: 20%;
+	border-top-left-radius: 20%;
+	padding: 32px;
+	padding-top: 160px;
+
+	@media ${mediaBreakpoint.down.md} {
+		border-radius: 0%;
+	} ;
+`;
+
+const Header = styled.h1`
+	font-family: TT_Ramilas;
+	font-size: 70px;
+	font-weight: 700;
+	line-height: 70px;
+	z-index: 3;
+	& span {
+		font-family: Poppins;
+	}
+`;
+const Subheader = styled.h2`
+	font-size: 28px;
+	font-weight: 800;
+	line-height: 28px;
+`;
+const Text = styled.p`
+	font-size: 18px;
+	line-height: 24px;
+	font-weight: 300;
+`;
+
+const StyledSemi = styled(SemiCircle)`
+	top: 0;
+	position: absolute;
+	transform: rotate(180deg);
+	width: 235px;
+	height: 120px;
+	border: 30px solid #fffbf0;
+	background: #88ccf9;
+	border-bottom: none;
+`;
+
+const OutermostContainer = styled.div`
+	padding-top: 81px;
+	min-height: 100vh;
+
+	@media ${mediaBreakpoint.down.md} {
+		padding: 0;
+	}
+`;
+const Container = styled.div`
+	max-width: 1320px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const SuccessSemnas = () => {
+	return (
+		<OutermostContainer className="bg-cream d-flex ">
+			<OuterContainer className="bg-orange w-100 d-flex flex-column align-items-center position-relative">
+				<StyledSemi />
+
+				<Container>
+					<Header className="text-cream text-center">
+						THANK YOU <span>!</span>
+					</Header>
+					<Subheader className="my-4">See you in November</Subheader>
+
+					<Text className="mt-2 text-center">
+						<i>
+							*If there is no error in filliing the form, within 2x24 hours you
+							will get a verification email (
+							<u>please also check your spam folder</u>), contain link and
+							password for join the seminar *If within 2x24 hours you do not
+							receive the verification email from the committee, please contact
+							us
+						</i>
+					</Text>
+				</Container>
+			</OuterContainer>
+		</OutermostContainer>
+	);
+};
+
+export default SuccessSemnas;
--- pages/national-seminar/register/sharing-session.js
+++ pages/national-seminar/register/sharing-session.js
@@ -6,6 +6,9 @@ import FormInput from "components/FormInput";
 import { mediaBreakpoint } from "utils/mediaBreakpoints";
 import MyButton from "components/Buttons/MyButton";
 import Select from "components/FormInput/Select";
+import SuccessSemnas from "components/Success/SuccessSemnas";
+
+import { validEmail, whitespace } from "utils/validations";
 
 const OuterContainer = styled.div`
 	min-height: 100vh;
@@ -65,6 +68,7 @@ const InnerContainer = styled.div`
 	@media ${mediaBreakpoint.down.md} {
 		border-radius: 0;
 		padding: 16px;
+		padding-bottom: 32px;
 	}
 `;
 
@@ -79,29 +83,6 @@ const Header = styled.h1`
 	font-size: 42px;
 	line-height: 42px;
 	letter-spacing: 0.3px;
-`;
-
-const LinkContainer = styled.div`
-	padding: 8px 32px;
-	border-radius: 80px;
-	display: flex;
-	width: 270px;
-
-	&:hover {
-		cursor: pointer;
-	}
-`;
-
-const StyledLink = styled.a`
-	font-size: 24px;
-	font-height: 24px;
-	text-transform: uppercase;
-	text-decoration: none;
-	font-weight: 500;
-	font-family: Poppins;
-	& a:hover {
-		color: inherit;
-	}
 `;
 
 const SubmitButton = styled(MyButton)`
@@ -138,6 +119,7 @@ const SharingSessionRegister = () => {
 		phoneNr: "",
 	});
 	const [loading, setLoading] = useState(false);
+	const [success, setSuccess] = useState(false);
 
 	const onChange = (e) => {
 		setDetails({ ...details, [e.target.name]: e.target.value });
@@ -183,102 +165,123 @@ const SharingSessionRegister = () => {
 		setLoading(false);
 	};
 
+	const handleSubmit = async () => {
+		if (
+			!validEmail(email) ||
+			whitespace(fullName) ||
+			whitespace(univName) ||
+			whitespace(phoneNr) ||
+			whitespace(dob)
+		) {
+			alert("Please fill in all required fields! 😊");
+		} else {
+			await registerDBCCParticipant();
+		}
+	};
+
 	return (
-		<OuterContainer className="bg-secondary">
-			<InnerContainer className="bg-cream d-flex justify-content-center shadow">
-				<Container className="d-flex flex-column align-items-center">
-					<Header className="mt-md-2 mb-md-4 mt-1 mb-3 text-center">
-						REGISTER{" "}
-						<span className="text-danger">
-							FOR <br /> SHARING SESSION 1.0
-						</span>
-					</Header>
+		<>
+			{success ? (
+				<SuccessSemnas />
+			) : (
+				<OuterContainer className="bg-secondary">
+					<InnerContainer className="bg-cream d-flex justify-content-center shadow">
+						<Container className="d-flex flex-column align-items-center">
+							<Header className="mt-md-2 mb-md-4 mt-1 mb-3 text-center">
+								REGISTER{" "}
+								<span className="text-danger">
+									FOR <br /> SHARING SESSION 1.0
+								</span>
+							</Header>
 
-					<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form">
-						<FormInput
-							onChange={onChange}
-							text={"Full Name*"}
-							name={"fullName"}
-							className="align-items-center"
-							required
-							value={fullName}
-						/>
-						<FormInput
-							onChange={onChange}
-							text={"Faculty / Department / Batch"}
-							name={"facultyDepartmentBatch"}
-							className="align-items-center"
-							placeholder=""
-							required
-							value={facultyDepartmentBatch}
-						/>
-					</div>
+							<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form">
+								<FormInput
+									onChange={onChange}
+									text={"Full Name*"}
+									name={"fullName"}
+									className="align-items-center"
+									required
+									value={fullName}
+								/>
+								<FormInput
+									onChange={onChange}
+									text={"Faculty / Department / Batch"}
+									name={"facultyDepartmentBatch"}
+									className="align-items-center"
+									placeholder=""
+									required
+									value={facultyDepartmentBatch}
+								/>
+							</div>
 
-					<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form ">
-						<Select
-							text="Status"
-							onChange={onChange}
-							name="status"
-							className="align-items-center"
-							content={statuses}
-						/>
+							<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form ">
+								<Select
+									text="Status"
+									onChange={onChange}
+									name="status"
+									className="align-items-center"
+									content={statuses}
+								/>
 
-						<FormInput
-							onChange={onChange}
-							text={"Email*"}
-							name={"email"}
-							placeholder="mail@example.com"
-							className="align-items-center"
-							required
-							value={email}
-						/>
-					</div>
+								<FormInput
+									onChange={onChange}
+									text={"Email*"}
+									name={"email"}
+									placeholder="mail@example.com"
+									className="align-items-center"
+									required
+									value={email}
+								/>
+							</div>
 
-					<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form ">
-						<FormInput
-							onChange={onChange}
-							text={"University / Institution*"}
-							name={"univName"}
-							className="align-items-center"
-							value={univName}
-						/>
-						<FormInput
-							onChange={onChange}
-							text={"Phone Number*"}
-							name={"phoneNr"}
-							className="align-items-center"
-							placeholder=""
-							required
-							value={phoneNr}
-						/>
-					</div>
+							<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form ">
+								<FormInput
+									onChange={onChange}
+									text={"University / Institution*"}
+									name={"univName"}
+									className="align-items-center"
+									value={univName}
+								/>
+								<FormInput
+									onChange={onChange}
+									text={"Phone Number*"}
+									name={"phoneNr"}
+									className="align-items-center"
+									placeholder=""
+									required
+									value={phoneNr}
+								/>
+							</div>
 
-					<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form mb-5">
-						<FormInput
-							onChange={onChange}
-							text={"Date of Birth*"}
-							name={"dob"}
-							className="align-items-center small"
-							value={dob}
-						/>
-					</div>
+							<div className="d-flex flex-lg-row flex-md-row flex-column w-100 form mb-5">
+								<FormInput
+									onChange={onChange}
+									text={"Date of Birth*"}
+									name={"dob"}
+									placeholder={"DD/MM/YYYY"}
+									className="align-items-center small"
+									value={dob}
+								/>
+							</div>
 
-					{loading ? (
-						<NoticeText>Registering...</NoticeText>
-					) : (
-						<SubmitButton
-							onClick={registerDBCCParticipant}
-							variant="danger"
-							textColor="cream"
-						>
-							<b>
-								<i>SUBMIT</i>
-							</b>
-						</SubmitButton>
-					)}
-				</Container>
-			</InnerContainer>
-		</OuterContainer>
+							{loading ? (
+								<NoticeText>Registering...</NoticeText>
+							) : (
+								<SubmitButton
+									onClick={handleSubmit}
+									variant="danger"
+									textColor="cream"
+								>
+									<b>
+										<i>SUBMIT</i>
+									</b>
+								</SubmitButton>
+							)}
+						</Container>
+					</InnerContainer>
+				</OuterContainer>
+			)}
+		</>
 	);
 };
 
